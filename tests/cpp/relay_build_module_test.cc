fd182ce83a1ff728c2f2fc7e58864bca8c66ac47
hlu1 <haolu7@gmail.com>
2019-05-30 21:30:01 -0700

    [Relay] Fix indirect memory leak in Relay interpreter

--- tests/cpp/relay_build_module_test.cc
+++ tests/cpp/relay_build_module_test.cc
@@ -29,11 +29,29 @@
 #include <tvm/runtime/registry.h>
 #include <tvm/packed_func_ext.h>
 
+#include <mutex>
 
+namespace{
 TVM_REGISTER_GLOBAL("test.sch")
 .set_body([](tvm::TVMArgs args, tvm::TVMRetValue *rv) {
   *rv = topi::generic::schedule_injective(args[0], args[1]);
   });
+
+void relayRegisterOps() {
+  static std::once_flag flag;
+  std::call_once(flag, []() {
+    auto reg = tvm::runtime::Registry::Get("relay.op._Register");
+    if (!reg) {
+      LOG(FATAL) << "no _Register";
+    }
+    auto s_i = tvm::runtime::Registry::Get("test.sch");
+    if (!s_i) {
+      LOG(FATAL) << "no _Register";
+    }
+    (*reg)("add", "FTVMSchedule", *s_i, 10);
+  });
+}
+}
 
 TEST(Relay, BuildModule) {
   using namespace tvm;
@@ -49,9 +67,9 @@ TEST(Relay, BuildModule) {
   auto B = tvm::runtime::NDArray::Empty({2, 3}, {kDLFloat, 32, 1}, {kDLCPU, 0});
   auto C = tvm::runtime::NDArray::Empty({2, 3}, {kDLFloat, 32, 1}, {kDLCPU, 0});
 
-  auto pA = (float*)A.ToDLPack()->dl_tensor.data;
-  auto pB = (float*)B.ToDLPack()->dl_tensor.data;
-  auto pC = (float*)C.ToDLPack()->dl_tensor.data;
+  auto pA = (float*)A->data;
+  auto pB = (float*)B->data;
+  auto pC = (float*)C->data;
 
   for (int i = 0; i < 6; ++i) {
     pA[i] = i;
@@ -59,15 +77,8 @@ TEST(Relay, BuildModule) {
     pC[i] = i + 2;
   }
   // get schedule
-  auto reg = tvm::runtime::Registry::Get("relay.op._Register");
-  auto s_i = tvm::runtime::Registry::Get("test.sch");
-  if (!reg) {
-    LOG(FATAL) << "no _Register";
-  }
-  if (!s_i) {
-    LOG(FATAL) << "no _Register";
-  }
-  (*reg)("add", "FTVMSchedule", *s_i, 10);
+  relayRegisterOps();
+
   // build
   auto pfb = tvm::runtime::Registry::Get("relay.build_module._BuildModule");
   tvm::runtime::Module build_mod = (*pfb)();
@@ -92,7 +103,82 @@ TEST(Relay, BuildModule) {
   set_input_f("c", C);
   run_f();
   tvm::runtime::NDArray Y = get_output_f(0);
-  auto pY = (float*)Y.ToDLPack()->dl_tensor.data;
+  auto pY = (float*)Y->data;
+  for (int i = 0; i < 6; ++i) {
+    CHECK_LT(fabs(pY[i] - (i + (i + 1) + (i + 2))), 1e-4);
+  }
+}
+
+TEST(Relay, FoldConstant) {
+  using namespace tvm;
+  auto tensor_type = relay::TensorTypeNode::make({2, 3}, ::tvm::Float(32));
+  auto a = relay::VarNode::make("a", tensor_type);
+  auto b = relay::VarNode::make("b", tensor_type);
+  auto add_op = relay::Op::Get("add");
+  auto x = relay::CallNode::make(add_op, {a, b}, tvm::Attrs(), {});
+  auto c = relay::VarNode::make("c", tensor_type);
+  auto y = relay::CallNode::make(add_op, {x, c}, tvm::Attrs(), {});
+  auto func = relay::FunctionNode::make(relay::FreeVars(y), y, relay::Type(), {});
+  auto A = tvm::runtime::NDArray::Empty({2, 3}, {kDLFloat, 32, 1}, {kDLCPU, 0});
+  auto B = tvm::runtime::NDArray::Empty({2, 3}, {kDLFloat, 32, 1}, {kDLCPU, 0});
+  auto C = tvm::runtime::NDArray::Empty({2, 3}, {kDLFloat, 32, 1}, {kDLCPU, 0});
+
+  auto pA = (float*)A->data;
+  auto pB = (float*)B->data;
+  auto pC = (float*)C->data;
+
+  for (int i = 0; i < 6; ++i) {
+    pA[i] = i;
+    pB[i] = i + 1;
+    pC[i] = i + 2;
+  }
+  // get schedule
+  relayRegisterOps();
+
+  // build
+  auto pfb = tvm::runtime::Registry::Get("relay.build_module._BuildModule");
+  tvm::runtime::Module build_mod = (*pfb)();
+  auto build_f = build_mod.GetFunction("build", false);
+  auto json_f = build_mod.GetFunction("get_graph_json", false);
+  auto mod_f = build_mod.GetFunction("get_module", false);
+  Map<tvm::Integer, tvm::Target> targets;
+  Target llvm_tgt = Target::create("llvm");
+  targets.Set(0, llvm_tgt);
+
+  // Pass a and b as parameters so they can be folded as constants
+  auto set_params_f = build_mod.GetFunction("set_params", false);
+  Map<std::string, tvm::relay::Constant> params;
+  params.Set("a", tvm::relay::ConstantNode::make(A));
+  params.Set("b", tvm::relay::ConstantNode::make(B));
+  set_params_f(params);
+
+  auto set_opt_level_f = build_mod.GetFunction("set_opt_level", false);
+  set_opt_level_f(2);
+
+  // auto disable_pass_f = build_mod.GetFunction("disable_pass", false);
+  // disable_pass_f("FoldConstant");
+
+  build_f(func, targets, llvm_tgt);
+  std::string json = json_f();
+  tvm::runtime::Module mod = mod_f();
+
+  auto get_params_f = build_mod.GetFunction("get_params", false);
+  const Map<std::string, tvm::relay::Constant> new_params = get_params_f();
+
+  // run
+  auto ctx = A->ctx;
+  auto pfr = tvm::runtime::Registry::Get("tvm.graph_runtime.create");
+  tvm::runtime::Module run_mod = (*pfr)(json, mod, (int)ctx.device_type, (int)ctx.device_id);
+  auto set_input_f = run_mod.GetFunction("set_input", false);
+  auto run_f = run_mod.GetFunction("run", false);
+  auto get_output_f = run_mod.GetFunction("get_output", false);
+  for (const auto& kv : new_params) {
+    set_input_f(kv.first, kv.second->data);
+  }
+  set_input_f("c", C);
+  run_f();
+  tvm::runtime::NDArray Y = get_output_f(0);
+  auto pY = (float*)Y->data;
   for (int i = 0; i < 6; ++i) {
     CHECK_LT(fabs(pY[i] - (i + (i + 1) + (i + 2))), 1e-4);
   }
